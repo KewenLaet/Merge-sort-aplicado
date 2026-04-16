8c8a27df23bd632f1dde005c9100ad340737bdba
KewenLaet <kewenlaet@gmail.com>
2026-04-16 16:57:53 -0300

    Refactor ordenar.h to include merge functions

--- ordenar.h
+++ ordenar.h
@@ -3,11 +3,15 @@
 
 #include "dadosDengue.h"
 
+// subfuncoes
+void criarVetorCopia(dadosDengue *tabela, tipoEscolha op, int *vCopia);
+void combinarChar(dadosDengue *tabela, int inicio, int meio, int fim);
+//void combinarInt(dadosDengue *tabela, int *vCopia, int inicio, int meio, int fim);
+
+// funcoes principais
 void escolhaOrdernarChar(dadosDengue *tabelaReg, tipoEscolha op);
 void escolhaOrdenarInt(dadosDengue *tabelaReg, tipoEscolha op);
-void criarVetorCopia(dadosDengue *tabela, tipoEscolha op, int *vCopia);
-// insertion temporario ate terminar o codigo em volta do merge
-void insertionSort(dadosDengue *tabelaReg, int tam);
-
+void mergeChar(dadosDengue *tabela, int inicio, int fim);
+//void mergeInt(dadosDengue *tabela, int *vCopia, int inicio, int fim);
 
 #endif
