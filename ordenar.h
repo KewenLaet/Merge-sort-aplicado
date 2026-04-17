65f89ee6b2b39c957865a5dc1d5d7ae1b371a051
KewenLaet <kewenlaet@gmail.com>
2026-04-17 18:58:11 -0300

    Versão pronta, apenas faltando exibirDados e possíveis extras.

--- ordenar.h
+++ ordenar.h
@@ -5,15 +5,10 @@
 
 // subfuncoes
 void criarVetorCopia(dadosDengue *tabelaReg, int inicio, int fim, int tipo, int *vCopia);
-int escificCompare(tipoEscolha op, dadosDengue *tabela, int *vCopia, int i, int j);
-//void combinarChar(dadosDengue *tabela, int inicio, int meio, int fim);
-//void combinarInt(dadosDengue *tabela, int *vCopia, int inicio, int meio, int fim);
+void combinar(dadosDengue *tabela, tipoEscolha op, int *vCopia, int inicio, int meio, int fim);
+int especificCompare(tipoEscolha op, dadosDengue *tabela, int *vCopia, int i, int j);
 
 // funcoes principais
-void escolhaOrdernarChar(dadosDengue *tabelaReg, tipoEscolha op);
-void escolhaOrdenarInt(dadosDengue *tabelaReg, tipoEscolha op);
 void mergeSort(dadosDengue *tabela, tipoEscolha op, int *vCopia, int inicio, int fim);
-//void mergeChar(dadosDengue *tabela, int inicio, int fim);
-//void mergeInt(dadosDengue *tabela, int *vCopia, int inicio, int fim);
 
 #endif
