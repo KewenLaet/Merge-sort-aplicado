9af995a23ba26155fb799dc123efb49602924d7f
KewenLaet <kewenlaet@gmail.com>
2026-04-17 16:25:02 -0300

    Refactor ordenar.h with new function signatures

--- ordenar.h
+++ ordenar.h
@@ -4,14 +4,16 @@
 #include "dadosDengue.h"
 
 // subfuncoes
-void criarVetorCopia(dadosDengue *tabela, tipoEscolha op, int *vCopia);
-void combinarChar(dadosDengue *tabela, int inicio, int meio, int fim);
+void criarVetorCopia(dadosDengue *tabelaReg, int inicio, int fim, int tipo, int *vCopia);
+int escificCompare(tipoEscolha op, dadosDengue *tabela, int *vCopia, int i, int j);
+//void combinarChar(dadosDengue *tabela, int inicio, int meio, int fim);
 //void combinarInt(dadosDengue *tabela, int *vCopia, int inicio, int meio, int fim);
 
 // funcoes principais
 void escolhaOrdernarChar(dadosDengue *tabelaReg, tipoEscolha op);
 void escolhaOrdenarInt(dadosDengue *tabelaReg, tipoEscolha op);
-void mergeChar(dadosDengue *tabela, int inicio, int fim);
+void mergeSort(dadosDengue *tabela, tipoEscolha op, int *vCopia, int inicio, int fim);
+//void mergeChar(dadosDengue *tabela, int inicio, int fim);
 //void mergeInt(dadosDengue *tabela, int *vCopia, int inicio, int fim);
 
 #endif
