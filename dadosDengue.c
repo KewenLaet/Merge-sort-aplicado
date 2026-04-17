3cdf5a0c61042546e048ce65d907b402781926df
KewenLaet <kewenlaet@gmail.com>
2026-04-17 18:57:39 -0300

    Versão pronta, apenas faltando exibirDados e possíveis extras.

--- dadosDengue.c
+++ dadosDengue.c
@@ -13,8 +13,7 @@ void lerDados(dadosDengue *tabelaRegiao, char *filepath, int qntUF){
     int n = 0, Gr23, Ob23, Gr24, Ob24, DIFF_Gr, DIFF_Ob;
     char reg[15], UF[3];
 
-    // possivelmente trocar o espaco por algum outro caractere como marcacao entre uma coluna e a outra, requeriria obviamente mudanca nos txt
-    while(fscanf(fp, "%[^;];%d;%d;%d;%d;%d;%d", UF, &Gr23, &Ob23, &Gr24, &Ob24, &DIFF_Gr, &DIFF_Ob) == 7 && n < qntUF){
+    while(fscanf(fp, "%[^;];%d;%d;%d;%d;%d;%d%*c", UF, &Gr23, &Ob23, &Gr24, &Ob24, &DIFF_Gr, &DIFF_Ob) == 7 && n < qntUF){
         strncpy(tabelaRegiao[n].UF, UF, 2);
         tabelaRegiao[n].UF[2] = '\0';
         tabelaRegiao[n].graves23 = Gr23;
@@ -32,14 +31,14 @@ void escreverDados(dadosDengue *tabelaReg, tipoEscolha op, int qntUF){ //-> desc
     FILE *arq = fopen("dadosOrdenados.txt", "w");
     int i;
 
-    fprintf(arq, "Regiao: %s", tabelaReg[op.reg].regiao);
+    //fprintf(arq, "Regiao: %s", tabelaReg[op.reg].regiao); criar uma funcao que troca o numero da regiao por uma string e devolve aqui
 
     for(i=0 ; i<qntUF ; i++){
         //No tipo que forem strings, passar o op escolhido pelo usuario no op.tipo
         fprintf(arq, " | UF: %s | Graves 23: %d | Graves 24: %d | Obitos 23: %d | Obitos 24: %d | DifGraves: %d | DifObitos: %d\n",
          tabelaReg[i].UF, tabelaReg[i].graves23, tabelaReg[i].graves24,
          tabelaReg[i].obitos23, tabelaReg[i].obitos24,
-         tabelaReg[i].difGraves, tabelaReg[i].difObitos);
+         tabelaReg[i].difGraves, tabelaReg[i].difObitos);// lembra de fazer um trem tipo isso = %-12s pra exibir de forma padrao
         //(char *)25 <-> cast
     }
     //fprintf aqui -> abrir o fopen do txt vazio
@@ -52,10 +51,10 @@ void escreverDados(dadosDengue *tabelaReg, tipoEscolha op, int qntUF){ //-> desc
 }*/
 int pegarDadosDeReg(int reg, char *caminho){
     const int ufRegiao[]= { 4, 9, 7, 4, 3 };
-    char const caminhoPt2[15][5]= { "dengueCentroOeste", "dengueNordeste", "dengueNorte", "dengueSudeste", "dengueSul" };
+    const char caminhoPt2[5][30]= { "dengueCentroOeste.txt", "dengueNordeste.txt", "dengueNorte.txt", "dengueSudeste.txt", "dengueSul.txt" };
 
-    strcpy(caminho, "./Arquivo das regiões/");
-    strcat(caminho, caminhoPt2[reg]);
+    strcpy(caminho, "ArquivosReg/");
+    strcat(caminho, caminhoPt2[reg-1]);
 
     return ufRegiao[reg-1];
 }
