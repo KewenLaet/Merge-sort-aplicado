f68515189f15ab1d8f5ef0fff630ca21d599501f
viniguitas <viniciusbarbosalopes@outlook.com>
2026-04-18 17:34:16 -0300

    Refactor data handling to use dadosGerais structure

--- dadosDengue.c
+++ dadosDengue.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void lerDados(dadosDengue *tabelaRegiao, char *filepath, int qntUF){
+void lerDados(dadosGerais *dados, char *filepath, int qntUF){
     FILE *fp = fopen(filepath, "r");
     if (fp == NULL){
         printf("\nERRO AO ABRIR ARQUIVO!\n");
@@ -11,23 +11,23 @@ void lerDados(dadosDengue *tabelaRegiao, char *filepath, int qntUF){
     }
 
     int n = 0, Gr23, Ob23, Gr24, Ob24, DIFF_Gr, DIFF_Ob;
-    char reg[15], UF[3];
+    char UF[3];
 
     while(fscanf(fp, "%[^;];%d;%d;%d;%d;%d;%d%*c", UF, &Gr23, &Ob23, &Gr24, &Ob24, &DIFF_Gr, &DIFF_Ob) == 7 && n < qntUF){
-        strncpy(tabelaRegiao[n].UF, UF, 2);
-        tabelaRegiao[n].UF[2] = '\0';
-        tabelaRegiao[n].graves23 = Gr23;
-        tabelaRegiao[n].obitos23 = Ob23;
-        tabelaRegiao[n].graves24 = Gr24;
-        tabelaRegiao[n].obitos24 = Ob24;
-        tabelaRegiao[n].difGraves = DIFF_Gr;
-        tabelaRegiao[n].difObitos = DIFF_Ob;
+        strncpy(dados->tabela[n].UF, UF, 2);
+        dados->tabela[n].UF[2] = '\0';
+        dados->tabela[n].graves23 = Gr23;
+        dados->tabela[n].obitos23 = Ob23;
+        dados->tabela[n].graves24 = Gr24;
+        dados->tabela[n].obitos24 = Ob24;
+        dados->tabela[n].difGraves = DIFF_Gr;
+        dados->tabela[n].difObitos = DIFF_Ob;
         n++;
     }
 
     fclose(fp);
 }
-void escreverDados(dadosDengue *tabelaReg, tipoEscolha op, int qntUF){ //-> descricao no arquivo dadosDengue.h
+void escreverDados(dadosGerais *dados, int qntUF){ //-> descricao no arquivo dadosDengue.h
     FILE *arq = fopen("dadosOrdenados.txt", "w");
     int i;
 
@@ -36,9 +36,9 @@ void escreverDados(dadosDengue *tabelaReg, tipoEscolha op, int qntUF){ //-> desc
     for(i=0 ; i<qntUF ; i++){
         //No tipo que forem strings, passar o op escolhido pelo usuario no op.tipo
         fprintf(arq, " | UF: %s | Graves 23: %d | Graves 24: %d | Obitos 23: %d | Obitos 24: %d | DifGraves: %d | DifObitos: %d\n",
-         tabelaReg[i].UF, tabelaReg[i].graves23, tabelaReg[i].graves24,
-         tabelaReg[i].obitos23, tabelaReg[i].obitos24,
-         tabelaReg[i].difGraves, tabelaReg[i].difObitos);// lembra de fazer um trem tipo isso = %-12s pra exibir de forma padrao
+         dados->tabela[i].UF, dados->tabela[i].graves23, dados->tabela[i].graves24,
+         dados->tabela[i].obitos23, dados->tabela[i].obitos24,
+         dados->tabela[i].difGraves, dados->tabela[i].difObitos);// lembra de fazer um trem tipo isso = %-12s pra exibir de forma padrao
         //(char *)25 <-> cast
     }
     //fprintf aqui -> abrir o fopen do txt vazio
