5257f468f0dec7032295b6be96ad3d1291f3a73c
Hiroguetsu <137731530+Hiroguetsu@users.noreply.github.com>
2026-04-22 18:52:13 -0300

    Refactor data handling functions for dengue data

--- dadosDengue.c
+++ dadosDengue.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
  
-void lerDados(dadosGerais *dados, char *filepath, int qntUF){
-    FILE *fp = fopen(filepath, "r");
+void lerDados(dadosGerais *dados){
+    FILE *fp = fopen(dados->op.path, "r");
     if (fp == NULL){
         printf("\nERRO AO ABRIR ARQUIVO!\n");
         return;
@@ -13,7 +13,7 @@ void lerDados(dadosGerais *dados, char *filepath, int qntUF){
     int n = 0, Gr23, Ob23, Gr24, Ob24, DIFF_Gr, DIFF_Ob;
     char UF[3];
  
-    while(fscanf(fp, " %[^;];%d;%d;%d;%d;%d;%d", UF, &Gr23, &Ob23, &Gr24, &Ob24, &DIFF_Gr, &DIFF_Ob) == 7 && n < qntUF){
+    while(fscanf(fp, "%[^;];%d;%d;%d;%d;%d;%d%*c", UF, &Gr23, &Ob23, &Gr24, &Ob24, &DIFF_Gr, &DIFF_Ob) == 7 && n < dados->op.qntUF){
         strncpy(dados->tabela[n].UF, UF, 2);
         dados->tabela[n].UF[2] = '\0';
         dados->tabela[n].graves23 = Gr23;
@@ -28,11 +28,11 @@ void lerDados(dadosGerais *dados, char *filepath, int qntUF){
     fclose(fp);
 }
  
-void escreverDados(dadosGerais *dados, int qntUF){
+void escreverDados(dadosGerais *dados){
     FILE *arq = fopen("dadosOrdenados.txt", "w");
     int i;
  
-    for(i=0; i<qntUF; i++){
+    for(i=0; i<dados->op.qntUF; i++){
         fprintf(arq, " | UF: %-12s | Graves 23: %-12d | Graves 24: %-12d | Obitos 23: %-12d | Obitos 24: %-12d | DifGraves: %-12d | DifObitos: %-12d\n",
          dados->tabela[i].UF, dados->tabela[i].graves23, dados->tabela[i].graves24,
          dados->tabela[i].obitos23, dados->tabela[i].obitos24,
@@ -41,23 +41,27 @@ void escreverDados(dadosGerais *dados, int qntUF){
     fclose(arq);
 }
  
-void exibirDados(dadosDengue *dados, int qntUF){
+void exibirDados(dadosGerais *dados){
     int i;
- 
-    for(i=0; i<qntUF; i++){
+
+    printf("\n\nRegiao: %s\n", dados->NomeReg);
+    for(i=0; i<dados->op.qntUF; i++){
         printf(" | UF: %-12s | Graves 23: %-12d | Graves 24: %-12d | Obitos 23: %-12d | Obitos 24: %-12d | DifGraves: %-12d | DifObitos: %-12d\n",
-         dados[i].UF, dados[i].graves23, dados[i].graves24,
-         dados[i].obitos23, dados[i].obitos24,
-         dados[i].difGraves, dados[i].difObitos);
+         dados->tabela[i].UF, dados->tabela[i].graves23, dados->tabela[i].graves24,
+         dados->tabela[i].obitos23, dados->tabela[i].obitos24,
+         dados->tabela[i].difGraves, dados->tabela[i].difObitos);
     }
 }
  
-int pegarDadosDeReg(int reg, char *caminho){
+int pegarDadosDeReg(int reg, char *caminho, char *nomeReg){
     const int ufRegiao[]= { 4, 9, 7, 4, 3 };
-    const char caminhoPt2[5][30]= { "dengueCentroOeste.txt", "dengueNordeste.txt", "dengueNorte.txt", "dengueSudeste.txt", "dengueSul.txt" };
- 
-    strcpy(caminho, "ArquivosReg/");
+    const char caminhoPt2[5][30]= { "Centro-Oeste", "Nordeste", "Norte", "Sudeste", "Sul" };
+
+    strcpy(caminho, "ArquivosReg/dengue");
     strcat(caminho, caminhoPt2[reg-1]);
- 
+    strcat(caminho, ".txt");
+
+    strcpy(nomeReg, caminhoPt2[reg-1]);
+
     return ufRegiao[reg-1];
 }
